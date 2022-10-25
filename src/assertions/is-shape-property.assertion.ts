da3eee38b60d54596023b59de7153291b98827c3
Archimedes of Syracuse <code@myth.software>
2022-10-25 12:32:56 -0700

    improve and export interfaces

--- src/assertions/is-shape-property.assertion.ts
+++ src/assertions/is-shape-property.assertion.ts
@@ -1,7 +1,11 @@
 export function assertsIsShapeProperty(value: unknown): asserts value is {
-  [key: string]: string | number;
+  [key: string]: string | number | string[];
 } {
   if (typeof value !== 'string' || typeof value !== 'number') {
+    throw new TypeError('not a shape property');
+  }
+
+  if (Array.isArray(value) && typeof value[0] !== 'string') {
     throw new TypeError('not a shape property');
   }
 }
