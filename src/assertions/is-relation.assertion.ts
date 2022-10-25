7953d57861889dc8dcb969643b0b67399af2d17e
Archimedes of Syracuse <code@myth.software>
2022-10-25 16:44:49 -0700

    fix relation assertion

--- src/assertions/is-relation.assertion.ts
+++ src/assertions/is-relation.assertion.ts
@@ -2,10 +2,12 @@ export function assertsIsRelation(
   value: unknown
 ): asserts value is { id: string }[] {
   if (!Array.isArray(value)) {
+    console.error(value);
     throw new TypeError('expected relations not in an array');
   }
 
-  if (typeof value[0] !== 'object' && !Object.keys(value[0]).includes('id')) {
+  if (typeof value[0] === 'object' && !Object.keys(value[0])?.includes('id')) {
+    console.error(value);
     throw new TypeError('expected relation does not include id property');
   }
 }
