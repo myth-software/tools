66caf73ebe0a1e51538047a4a80a60c361a405f3
Archimedes of Syracuse <code@myth.software>
2022-10-25 17:02:12 -0700

    flatten multi-select

--- src/assertions/is-relation.assertion.ts
+++ src/assertions/is-relation.assertion.ts
@@ -3,11 +3,11 @@ export function assertsIsRelation(
 ): asserts value is { id: string }[] {
   if (!Array.isArray(value)) {
     console.error(value);
-    throw new TypeError('expected relations not in an array');
+    throw new TypeError('relations must be an array');
   }
 
   if (typeof value[0] === 'object' && !Object.keys(value[0])?.includes('id')) {
     console.error(value);
-    throw new TypeError('expected relation does not include id property');
+    throw new TypeError('relation object must include "id" property');
   }
 }
