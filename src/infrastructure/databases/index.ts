a26b29d4bd2610f8046d0e498b02315445bdb17c
Peter B Smith <1965474+peterbsmyth@users.noreply.github.com>
2022-10-31 17:43:01 -0700

    emulate notion structure

---
+++ src/infrastructure/databases/index.ts
@@ -0,0 +1,7 @@
+export * from './databases-query';
+export * from './databases-query-all';
+export * from './databases-query-all-results';
+export * from './databases-query-results';
+export * from './databases-retrieve';
+export * from './databases-retrieve-all';
+export * from './databases-update';
---
+++ src/infrastructure/pages/index.ts
@@ -0,0 +1,4 @@
+export * from './pages-create';
+export * from './pages-properties-retrieve';
+export * from './pages-retrieve';
+export * from './pages-update';
